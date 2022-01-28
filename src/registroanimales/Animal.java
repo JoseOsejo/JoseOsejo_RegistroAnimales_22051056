acc5e2a34264c9d6e0a354c5e73d4ee137c497c9
JoseOsejo <80367652+JoseOsejo@users.noreply.github.com>
2022-01-28 14:44:26 -0600

    constructor mutadores y toString de clase Animal

--- src/registroanimales/Animal.java
+++ src/registroanimales/Animal.java
@@ -18,6 +18,86 @@ public class Animal
     private String descripcionRasgos;
     private String distribucionGeografica;
     private int vida;//debe ser mayor a cero
+
+    public Animal(String nombreCientifico, String nombreComun, String habitat, String alimentacion, String descripcionRasgos, String distribucionGeografica, int vida) {
+        this.nombreCientifico = nombreCientifico;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.descripcionRasgos = descripcionRasgos;
+        this.distribucionGeografica = distribucionGeografica;
+        this.vida = vida;
+    }
+
+    public Animal() {
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getDescripcionRasgos() {
+        return descripcionRasgos;
+    }
+
+    public void setDescripcionRasgos(String descripcionRasgos) {
+        this.descripcionRasgos = descripcionRasgos;
+    }
+
+    public String getDistribucionGeografica() {
+        return distribucionGeografica;
+    }
+
+    public void setDistribucionGeografica(String distribucionGeografica) {
+        this.distribucionGeografica = distribucionGeografica;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "nombreCientifico=" + nombreCientifico 
+                + ", nombreComun=" + nombreComun 
+                + ", habitat=" + habitat
+                + ", alimentacion=" + alimentacion 
+                + ", descripcionRasgos=" + descripcionRasgos 
+                + ", distribucionGeografica=" + distribucionGeografica 
+                + ", vida=" + vida + '}';
+    }
     
     
     
